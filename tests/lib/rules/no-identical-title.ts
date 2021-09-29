16d5e5020cff3491588c69431287e72920b30f54
codejedi365 <codejedi365@gmail.com>
2021-09-28 22:40:19 -0400

    Fix: no-identical-titles TS implementation
    \### Rationale
    Remove explicit `any` types since it defeats the purpose of using
    Typescript. When resolving, there are some edge-cases that would of
    caused this plugin to crash `eslint`. Added error handling for these
    edge cases and added rule tests to verify the error handling prevents
    an `eslint` crash.

--- tests/lib/rules/no-identical-title.ts
+++ tests/lib/rules/no-identical-title.ts
@@ -1,31 +1,36 @@
+import resolveFrom from "resolve-from";
+import { TSESLint } from "@typescript-eslint/experimental-utils";
 import rule from "../../../lib/rules/no-identical-title";
-import {RuleTester} from 'eslint';
-
-
-import resolveFrom from 'resolve-from';
-import { TSESLint } from '@typescript-eslint/experimental-utils';
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
-let ruleTester = new TSESLint.RuleTester({     parser: resolveFrom(require.resolve('eslint'), 'espree'), parserOptions: { ecmaVersion: 8 } });
+const ruleTester = new TSESLint.RuleTester({
+    parser: resolveFrom(require.resolve("eslint"), "espree"),
+    parserOptions: { ecmaVersion: 8 }
+});
 
-let message = 'Do not use the `.debug` action.';
-ruleTester.run("no-debug", rule, {
-    valid:[
+ruleTester.run("noIdenticalTitles", rule, {
+    valid: [
         `
         test("my test", async t => {});
         test("my other test", async t => {});
-        `
+        `,
+        // ignore undeterminable code (don't throw an error, not this rule's focus)
+        `test(/^regex_name/, async t => {});`,
+        `test(null, async t => {})`,
+        `test(getTestName("F-0001"), async t => {});`
     ],
     invalid: [
         {
-        code: `
-        test("my test", async t => {});
-        test("my test", async t => {});
-        `,
-        errors: [{messageId: "noIdenticalTitles"},{messageId: "noIdenticalTitles"}]
+            code: `
+                test("my test", async t => {});
+                test("my test", async t => {});
+            `,
+            errors: [
+                { messageId: "noIdenticalTitles" },
+                { messageId: "noIdenticalTitles" }
+            ]
         }
     ]
 });
-
