be4d75cc586f55b33f2755b04789fe89b356496e
codejedi365 <codejedi365@gmail.com>
2021-10-13 05:57:18 -0400

    integration-tests > Add jest-serial-runner config
    \### Rationale
    Need to add a configuration to manipulate the parallelizing normal jest
    uses to run.

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,37 @@
 module.exports = {
-    testMatch: ["**/tests/**/*.ts"],
-    collectCoverage: true,
     preset: "ts-jest",
-    coverageThreshold: {
-        "global": {
-            branches: 90,
-            functions: 100,
-            lines: 90,
-            statements: 90
+    projects: [
+        {
+            displayName: "unit-tests",
+            testMatch: ["tests/**/*.test.ts"],
+            collectCoverage: true,
+            coverageThreshold: {
+                "global": {
+                    branches: 90,
+                    functions: 100,
+                    lines: 90,
+                    statements: 90
+                },
+                "./lib/rules/expect-expect.ts": {
+                    // allow error handling
+                    lines: -3,
+                    statements: -5
+                }
+            },
+            collectCoverageFrom: [
+                "lib/**/*.ts",
+                "index.ts",
+                "!**/node_modules/**"
+            ]
         },
-        "./lib/rules/expect-expect.ts": {
-            // allow error handling
-            lines: -3,
-            statements: -5
+        {
+            // Tests to run in serial
+            displayName: "integration-tests",
+            collectCoverage: false,
+            runner: "jest-serial-runner",
+            // testEnvironment: "node",
+            testMatch: ["tests/**/*.serial-test.ts"]
         }
-    },
-    testPathIgnorePatterns: ["<rootDir>/tests/fixtures/"],
-    collectCoverageFrom: ["lib/**/*.ts", "index.ts", "!**/node_modules/**"]
+    ],
+    testPathIgnorePatterns: ["<rootDir>/tests/fixtures/"]
 };
