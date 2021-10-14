228bdd0d0b9e21d23010f42b57c6a41c54d17ebe
codejedi365 <codejedi365@gmail.com>
2021-10-14 19:49:16 -0400

    integration-tests > add lint errors that should be flagged

--- example/example.test.ts
+++ example/example.test.ts
@@ -7,7 +7,14 @@ const getBrowserURL = ClientFunction(() => document.location.href);
 fixture("MyHTMLPage")
     .page(`file://${path.join(__dirname, "src", "index.html")}`);
 
-test("Page loads and displays hello world", async (t: TestController) => {
+// Lint Error: noOnly
+test.only("Page loads and displays hello world", async (t: TestController) => {
     await t.expect(getBrowserURL()).match(/file:\/\/.+\/src\/index.html/);
     await t.expect(Selector("p").withText("Hello World")).ok();
 });
+
+// Lint Error: noSkip, noIdenticalTitle, expectExpect
+test.skip("Page loads and displays hello world", async (t: TestController) => {
+    // Lint Error: noDebug
+    t.debug()
+})
