e224d958c73130e5974044b4e30e4d5e524869a4
codejedi365 <codejedi365@gmail.com>
2021-11-03 11:58:06 -0400

    test(pkg.spec): revert templated entry for version specification

--- tests/pkg.spec.ts
+++ tests/pkg.spec.ts
@@ -1,28 +1,12 @@
-import { promisify } from "util";
-import { exec } from "child_process";
-import { coerce, major, validRange } from "semver";
+import { validRange } from "semver";
 import thisModule from "../package.json";
-
-const execProcess = promisify(exec);
 
 describe("package.json", () => {
     it("should have valid semver range for the eslint peerDependency", () => {
         expect(validRange(thisModule.peerDependencies.eslint)).toBeTruthy();
     });
 
-    it("should not be updated manually (unless major version tag found)", async () => {
-        let majorVersion = "[0-9]+";
-        try {
-            const result = await execProcess(
-                ["git", "describe", "--tags", "--abbrev=0"].join(" ")
-            );
-            const tmp = coerce(result.stdout);
-            majorVersion = !tmp ? majorVersion : major(tmp.version).toString();
-        } catch (err) {
-            // Ignore, git probably wasn't found. allow any number.
-        }
-        expect(thisModule.version).toMatch(
-            new RegExp(`${majorVersion}\\.0\\.0-autoversioned`)
-        );
+    it("should not be updated manually", () => {
+        expect(thisModule.version).toEqual("0.0.0-semantically-versioned");
     });
 });
