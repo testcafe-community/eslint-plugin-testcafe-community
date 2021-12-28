42816c1acf0a23803517926867e27b575861688e
codejedi365 <codejedi365@gmail.com>
2021-12-28 06:09:41 +0000

    test(docs): add test to ensure lint is applied to each rule's docs

--- tests/docs/rules.spec.ts
+++ tests/docs/rules.spec.ts
@@ -1,7 +1,12 @@
-import { access } from "fs-extra";
+import { access, readdirSync } from "fs-extra";
 import { resolve } from "path";
+import { exec as execCallback } from "child_process";
+import { promisify } from "util";
 import rulebook from "../../lib/rules";
 import type { TestCafeLint } from "../../lib/globals";
+
+const SECONDS = 1000;
+const exec = promisify(execCallback);
 
 describe("Rules", () => {
     const ruleNames = Object.keys(rulebook.rules) as TestCafeLint.RuleName[];
@@ -17,5 +22,25 @@ describe("Rules", () => {
             );
             await expect(access(ruleDocFile)).toResolve();
         }
+    );
+});
+
+describe("Rule-doc", () => {
+    const getRuleDocFiles = (): string[] => {
+        const ruleDocsDir = resolve(__dirname, "..", "..", "docs", "rules");
+        const allFilesInDocsDir = readdirSync(ruleDocsDir);
+        return allFilesInDocsDir
+            .filter((filename) => /(.*\.md)$/.test(filename))
+            .map((filename) => resolve(ruleDocsDir, filename));
+    };
+
+    it.each(getRuleDocFiles())(
+        "%s should be properly formatted markdown",
+        async (ruleDoc) => {
+            await expect(
+                exec(`npx eslint --ext md "${ruleDoc}"`)
+            ).resolves.toBeTruthy();
+        },
+        30 * SECONDS
     );
 });
