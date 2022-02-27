9f3c2667daf58843371fcd8272c55861730a80be
codejedi365 <codejedi365@gmail.com>
2022-02-27 00:38:48 +0000

    chore(git-hooks): update `activate_nvm_env` to support partial node version declarations
    Allow more generic pinned version that will likely not cause a conflict and allow less forced
    updating of versions

--- .husky/hook-utils.sh
+++ .husky/hook-utils.sh
@@ -97,8 +97,9 @@ config_git_commit_signing() {
 # Function to activate designated node environment based on .nvmrc value
 # facilitates installation if not existant
 activate_nvm_env() {
-  # Check if NVM is installed
   if ! command -v nvm >/dev/null 2>&1; then
+    # check bash profile
+    if ! NVM_DIR="$(bash -lic 'command -v nvm &>/dev/null && echo "$NVM_DIR"')"; then
     NVM_DEFAULT_PATH="$HOME/.nvm/nvm.sh"
     if ! [ -s "$NVM_DEFAULT_PATH" ] || replay "$SHELL" | grep dash; then
       # Abort [1], nvm not found
@@ -107,19 +108,41 @@ activate_nvm_env() {
     fi
     # shellcheck disable=SC2034
     NVM_DIR="$(dirname "$NVM_DEFAULT_PATH")"
-    # shellcheck disable=SC1090,SC2240
-    . "$NVM_DEFAULT_PATH" --no-use # Load nvm into git hook's shell
+      unset -v NVM_DEFAULT_PATH
+    fi
+    # shellcheck disable=SC1090,SC1091,SC2240
+    . "$NVM_DIR/nvm.sh" --no-use # Load nvm into git hook's shell
   fi
 
-  currentNodeVer="$(nvm current)"
   projDir="$(get_project_dir)"
-  if ! [ -f "$projDir/.nvmrc" ] || grep -q "$currentNodeVer" "$projDir/.nvmrc"; then
-    unset -v projDir currentNodeVer
+  
+  if ! [ -f "$projDir/.nvmrc" ]; then
+    unset -v projDir
     nvm unload
-    return 0 # Matches current version, stop processing
+    return 0 # no found nvmrc file, ie no node version set
   fi
-  unset -v projDir currentNodeVer
 
+  # This section was developed for compatibility with nvm 0.38.0
+  # direct calls to internal non-api functions used, could be problematic with updates
+  currentNodeVer="$(nvm current | grep -v 'system' || node --version)"
+  NVM_RC_VERSION="$(cd "$projDir" 2>/dev/null && NVM_SILENT=1 nvm_rc_version && (printf '%s' "$NVM_RC_VERSION" || :;))"
+  MIN_DEV_VERSION="$(nvm_format_version "$NVM_RC_VERSION" || :;)"
+  MAX_DEV_VERSION="$(nvm version-remote "$NVM_RC_VERSION" || :;)"
+  unset -v NVM_RC_VERSION projDir
+
+  # abort if either calculation failed and values are empty, otherwise:
+  # MIN_DEV_VERSION <= currentVersion <= MAX_DEV_VERSION
+  if [ -z "$MIN_DEV_VERSION" ] || [ -z "$MAX_DEV_VERSION" ] || \
+    (nvm_version_greater_than_or_equal_to "$currentNodeVer" "$MIN_DEV_VERSION" \
+    && ! nvm_version_greater "$MAX_DEV_VERSION" "$currentNodeVer");
+  then
+    unset -v currentNodeVer MIN_DEV_VERSION MAX_DEV_VERSION
+    nvm unload
+    return 0 # matches allowed versions, stop processing
+  fi
+  unset -v currentNodeVer MIN_DEV_VERSION MAX_DEV_VERSION
+
+  # Install correct version
   output=""
   if ! output="$(nvm use 2>&1 1>/dev/null)"; then
     if ! replay "$output" | grep --quiet "not yet installed"; then
@@ -128,7 +151,7 @@ activate_nvm_env() {
       return 0 # Abort action
     fi
     VERSION=""
-    VERSION="$(replay "$output" | grep 'nvm install' | sed -E 's/.*install v([0-9]+\.[0-9]+\.[0-9]+).*/\1/')"
+    VERSION="$(replay "$output" | grep 'nvm install' | sed -E 's/.*install (v?[0-9]+(\.[0-9]+){0,2}).*/\1/')"
     # enable user input, git by default runs in non-interactive mode
     if ! exec </dev/tty; then
       unset -v output VERSION
